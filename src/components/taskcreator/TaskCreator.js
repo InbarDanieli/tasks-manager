2c837743f2ca6415d0cadd279d0806021802d8f0
Inbar Danieli <inbar0308@gmail.com>
2021-12-15 14:27:33 +0200

    Added style

--- src/components/taskcreator/TaskCreator.js
+++ src/components/taskcreator/TaskCreator.js
@@ -22,29 +22,33 @@ function TaskCreator(props) {
 
   function ReturnItem() {
     if (task && description && date && time !== "") {
-      
-        props.fullitem({ task, description, date: new Date(date + " " + time), })
-        setDescription("")
-        setTask("")
-        setDate("")
-        setTime("")
-        props.onExit()
-      }
-      else
-      {
-        return alert("make sure you added all the information needed")
-      }
+
+      props.fullitem({ task, description, date: new Date(date + " " + time), })
+      setDescription("")
+      setTask("")
+      setDate("")
+      setTime("")
+      props.onExit()
     }
-  
+    else {
+      return alert("make sure you added all the information needed")
+    }
+  }
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
-        <button onClick={props.onExit}> X </button>
-        <input type="text" placeholder="write your task" onChange={GetTask} value={task}></input>
-        <input type="text" placeholder="description..." onChange={GetDescription} value={description}></input>
-        <input type="date" onChange={GetDate} value={date}></input>
-        <input type="time" onChange={GetTime} value={time}></input>
-        <button onClick={ReturnItem}>Save</button>
+        <button className="closeButton" onClick={props.onExit}> <b> X </b> </button>
+        <input className="TaskTitle" type="text" placeholder="write your task" onChange={GetTask} value={task}></input>
+        <textarea className="TaskText" placeholder="description..." onChange={GetDescription} value={description}></textarea>
+        <div>
+          <input className="TaskDate" type="date" onChange={GetDate} value={date}></input>
+          <input className="TaskTime" type="time" onChange={GetTime} value={time}></input>
+        </div>
+        <div>
+          <button className="saveButton" onClick={ReturnItem}>Save</button>
+          <button className="cancelButton" onClick={props.onExit}> cancel </button>
+        </div>
       </div>
     </div>
   )
