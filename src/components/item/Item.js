efb660b499519bc21a321b15e0f3c7d80ce18376
InbarDanieli <inbar0308@gmail.com>
2021-12-20 11:24:42 +0200

    Added trash icon

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Item.css"
+import trashIcon from "../../Assets/trash-alt-regular.svg"
 
 function Item(props) {
 
@@ -7,11 +8,11 @@ function Item(props) {
     <div className="Item">
       <li className="List">
         <h1 className="Title"><span>{props.item.task}</span></h1>
-        <button className="CloseButton" onClick={props.onDelete}> <b> X </b> </button>
         <p className = "DateAndTime"> {Intl.DateTimeFormat('en-GB', { dateStyle: "medium", timeStyle: "short" }).format(props.item.date)} </p>
         <div className="BodyText">
           <p>{props.item.description}</p>
         </div>
+        <button className="TrashButton" onClick={props.onDelete}><img src={trashIcon} alt="Trash Button" width={"20px"}/></button>
       </li>
     </div>)
 }
