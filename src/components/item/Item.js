ff0f7cc15ff60d41a3128487aa558e9ea3320a65
InbarDanieli <inbar0308@gmail.com>
2022-02-03 16:52:06 +0200

    Changed:(Item.js)-designe

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./Item.css"
-import {FaTrashAlt} from "react-icons/fa"
+import { FaTrashAlt } from "react-icons/fa"
 // import trashIcon from "../../Assets/trash-alt-regular.svg"
 
 /**
@@ -11,17 +11,16 @@ import {FaTrashAlt} from "react-icons/fa"
 function Item(props) {
 
   return (
-      <li className="taskContainer">
-        <h1 className="Title"><span>{props.item.task}</span></h1>
-        <button className="DeleteButton" onClick={props.onDelete}> <FaTrashAlt/> </button>
-        <div className="colorTag" style={{backgroundColor: `${props.item.bordercolor!=="transparent"?`var(--${props.item.bordercolor}color)`:"transparent"}`}}></div>
-        <p className = "DateAndTime" style = {{display: props.item.removedate}}> {Intl.DateTimeFormat('en-GB', { dateStyle: "medium", timeStyle: "short" }).format(props.item.date)} </p>
-        <div className="descriptionText">
-          <p>{props.item.description}</p>
-
-        </div>
-      </li>
-    )
+    <li className="taskContainer">
+      <h1 className="Title"><span>{props.item.task}</span></h1>
+      <div className="colorTag" style={{ backgroundColor: `${props.item.bordercolor !== "transparent" ? `var(--${props.item.bordercolor}color)` : "transparent"}` }}></div>
+      <div className="descriptionText"> <p>{props.item.description}</p> </div>
+      <span>
+        <p className="DateAndTime" style={{ display: props.item.removedate }}> {Intl.DateTimeFormat('en-GB', { dateStyle: "medium", timeStyle: "short" }).format(props.item.date)} </p>
+        <button className="DeleteButton" onClick={props.onDelete}> <FaTrashAlt /> </button>
+      </span>
+    </li>
+  )
 }
 
 
