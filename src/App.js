f48d86f45c6ab33147ad2ef7cf21f13558abe822
InbarDanieli <inbar0308@gmail.com>
2022-03-08 15:20:14 +0200

    fix: first description

--- src/App.js
+++ src/App.js
@@ -2,42 +2,11 @@ import './App.css';
 import List from './components/list/List';
 import React, { useState } from 'react';
 import ButtonPopup from './components/popupButton/ButtonPopup';
+import { IntroDescription } from './IntroDescription';
+import Footer from './Footer/Footer';
 
 function App() {
-  const listitems = [
-    {
-      task: "Hello! 👋",
-      description: "And welcome to your little task-manager! \n\n in this site you can add your tasks to organize your everyday life 😄",
-      date: new Date(2018, 8, 5, 15, 30),
-      bordercolor: "transparent"
-    },
-    {
-      task: "How to use it? 🤔",
-      description: "You can add your task by clicking on the \"Add Task\" button and adding your task info \n\n pro tip!😎\n you can disable the date by switching the date button to \"off\", like in this task, the date is diable👆",
-      date: new Date(2012, 2, 7, 15, 30),
-      removedate: "none",
-      bordercolor: "transparent"
-    },
-    {
-      task: "colors 🌈",
-      description: "To make your tasks more organized by adding color tags \n 👈 like so",
-      date: new Date(2021, 11, 13, 15, 30),
-      removedate: "none",
-      bordercolor: "blue",
-    },
-    {
-      task: "Delete me! 👉",
-      description: "First step: you can delete tasks by clicking on the trash icon\n\nBUT⚠️!\n You cannot bring a task back after you Delete it, so make sure you delete only the task you're done with",
-      date: new Date(2021, 11, 13, 15, 30),
-      bordercolor: "transparent",
-    },
-    {
-      task: "Updates?😊",
-      description: "What you see is only the beginning!\nYou can go to my GitHub page (link below👇) to read the updates\n\n Hope you like it!❤️",
-      date: new Date(2021, 11, 13, 15, 30),
-      bordercolor: "transparent",
-    },
-  ]
+  const listitems = IntroDescription
 
   const [taskarr, setTaskarr] = useState(getLSitems() || listitems)
   const [editMode, setEditMode] = useState(false)
@@ -103,7 +72,7 @@ function App() {
     <div className="App">
       <div>
         <ButtonPopup
-          onExit={() => {setEditItem("")}}
+          onExit={() => { setEditItem("") }}
           itemValues={editItem}
           fullitem={(task) => { FullItemsHandler(task) }}
         />
@@ -114,15 +83,7 @@ function App() {
         onEdit={editHandler}
       />
 
-      <div className='footer'>
-        <span>created by inbar danieli </span>
-        <a
-          rel="noreferrer"
-          href='https://github.com/InbarDanieli/tasks-manager'
-          target="_blank">GitHub page
-        </a>
-        <button onClick={ResetApp}>reset</button>
-      </div>
+      <Footer onClick={ResetApp} />
     </div>
   );
 }
